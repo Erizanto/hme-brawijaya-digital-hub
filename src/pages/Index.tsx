448806dee542d7343dbbbff0215a86f47d7ee787
Erizanto <erizantovareza@gmail.com>
2025-10-20 14:07:49 +0700

    tombol

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -154,12 +154,6 @@ const Index = () => {
           </div>
         </div>
 
-        {/* Scroll Indicator */}
-        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
-          <div className="w-6 h-10 border-2 border-primary-foreground rounded-full flex justify-center">
-            <div className="w-1 h-3 bg-primary-foreground rounded-full mt-2"></div>
-          </div>
-        </div>
       </section>
 
 
